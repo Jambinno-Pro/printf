cd3ed16193459971d01def53ddd7f72e1d1b5917
Lebokapi <125546711+Lebokapi@users.noreply.github.com>
2023-04-27 17:05:11 +0200

    Update function2.c

--- function2.c
+++ function2.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /****************** PRINT POINTER ******************/
 /**
+ * Innocent and Lebo
  * print_pointer - Prints the value of a pointer variable
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
